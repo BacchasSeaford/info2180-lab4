c23e4d97c72ea1cd92bec9feae6ba39e69bfb91c
BacchasSeaford <richybacchas0911@gmail.com>
2016-10-13 18:00:19 -0500

    exersice one

---
+++ maze.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+		var x = document.getElementById("boundary1");
+		x.addEventListener("mouseover", function changecolor(){
+		x.setAttribute('class', 'boundary youlose');}); 
+}
