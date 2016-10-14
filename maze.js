215059e6735364e16f36668ee00c2d3a625e9d25
BacchasSeaford <richybacchas0911@gmail.com>
2016-10-14 10:15:01 -0500

    exercise 2

--- maze.js
+++ maze.js
@@ -2,4 +2,17 @@ window.onload = function() {
 		var x = document.getElementById("boundary1");
 		x.addEventListener("mouseover", function changecolor(){
 		x.setAttribute('class', 'boundary youlose');}); 
+		
+		glow_Red();
+}
+
+function glow_Red(){
+	var wb = document.querySelectorAll("div.boundary");
+		for(var i = 0; i < wb.length; i++) {
+			wb[i].addEventListener('mouseover',function allwall(){
+			for (var k = 0; k < wb.length; k++){				
+			wb[k].setAttribute('class','boundary youlose');
+			}
+		});
+		}
 }
